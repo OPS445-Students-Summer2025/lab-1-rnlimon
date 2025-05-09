44b3a8a9b41a1b47e3444e5d48d522d2557e16a4
Raffy Limon <rnlimon@myseneca.ca>
2025-05-09 14:27:23 -0400

    Added lab1d

---
+++ lab1d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Integer objects
+x = 10
+y = 2
+z = 5
+
+# Calculation
+result = x + y * z
+
+# Print the output
+print (str(x) + ' + ' + str(y) + ' * ' + str(z) + ' = ' + str (result))
